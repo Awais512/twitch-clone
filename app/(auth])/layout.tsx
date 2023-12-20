8852893f8f259eef9b3807ebaae461b3171f3bf1
awais512 <raza.awais25@gmail.com>
2023-12-20 15:40:19 +0000

    Logo

--- app/(auth])/layout.tsx
+++ app/(auth])/layout.tsx
@@ -1,6 +1,11 @@
+import { Logo } from "./_components/logo";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full items-center justify-center flex">{children}</div>
+    <div className="h-full items-center justify-center space-y-6 flex flex-col">
+      <Logo />
+      {children}
+    </div>
   );
 };
 
